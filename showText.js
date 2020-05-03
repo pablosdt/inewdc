e8a1b89b18a1cfb81aa41ae525893f900987f7f3
Pablo Sáenz de Tejada <pablosdt@gmail.com>
2020-05-03 23:20:09 +0200

    Title update

--- showText.js
+++ showText.js
@@ -17,7 +17,7 @@ function listQ() {
     "https://servicios.ine.es/wstempus/js/ES/DATOS_METADATAOPERACION/HPT?g1=240:12365&g2=247:12418&g3=429:19947&g4=115:&p=1"
   ) {
     changedText.textContent =
-      "Importe total de hipotecas de viviendas constituidas en cada provincia en miles de euros. Es importante tener en cuenta que se trata de el importe TOTAL, es decir la suma del importe de todas las hipotecas en cada mes y en cada provincia, y que sólo se incluyen las hipotecas de viviendas excluyéndose otro tipo de fincas. El INE ofrece datos mensuales para esta serie estadística. Podría utilizarse junto con el número total de hipotecas a través de una fuente de datos adicional en Tableau para calcular el importe medio. Requerirá limpiar la columna del ine 'Variable' para separar el nombre de la provincia.";
+      "Importe total de hipotecas de viviendas constituidas en cada provincia en miles de euros. Es importante tener en cuenta que se trata de el importe TOTAL, es decir la suma del importe de todas las hipotecas en cada mes y en cada provincia, y que sólo se incluyen las hipotecas de viviendas excluyéndose otro tipo de fincas. El INE ofrece datos mensuales para esta serie estadística. Requerirá limpiar la columna del ine 'Variable' para separar el nombre de la provincia.";
   }
 }
 document.getElementById("selectURL").onchange = listQ;
